7fb27cb9e4806497f39c02230467435d5f6888e5
viquiiz <134979054+viquiiz@users.noreply.github.com>
2024-02-21 18:20:42 +0000

    initial commit

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,13 @@
-const buttons = document.querySelectorAll('.nav-button');
-const sections = document.querySelectorAll('section');
+/* Código que deixa a barra de navegação funcional */
+
+const buttons = document.querySelectorAll('.nav-button'); /* Seleciona todos os botões da barra de navegação */
+const sections = document.querySelectorAll('section'); /* Seleciona todas as 4 seções do site */
 
 buttons.forEach(button => {
 	button.addEventListener('click', () => {
-		const sectionId = button.getAttribute('data-section');
+		const sectionId = button.getAttribute('data-section'); /* Ao clicar em um botão, ele seleciona a seção correspondente */
 
+		/* Abaixo o código adiciona e remove classes para mostrar ou não uma seção, conforme o id que corresponde ao botão clicado */
 		sections.forEach(section => {
 			if (section.id === sectionId) {
 				section.classList.add('active');
